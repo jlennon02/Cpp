b796305bef851cd41bae4b8d86001553bc102357
miguelConchacalla <iair.suico@ucsp.edu.pe>
2022-09-02 07:13:50 -0500

    commit

--- semana2/helloworld.cpp
+++ semana2/helloworld.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main (){
     cout<<"hola mundo"<<endl;
+    int a;
+    int b;
+    cout<<"ingrese un valor para ""a"": ";
+    cin>>a;
+    cout<<"ingrese un valor para ""b"": ";
+    cin>>b;
+    int suma=a+b;
+    cout<<"el resultado es : "<<suma;
     return 0;
 }
