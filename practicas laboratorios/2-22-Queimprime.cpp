1dd841bed1a1924ba22cd77e915f0678a00ad97e
jairo champi <jairolenn123@gmail.com>
2022-09-05 22:06:34 -0500

    come

---
+++ practicas laboratorios/2-22-Queimprime.cpp
@@ -0,0 +1,21 @@
+/*
+¿que imprime el codigo?
+cout<<"*****\n****\n***\n**\n*\n"<<endl;
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"*****\n****\n***\n**\n*\n"<<endl;
+    return 0;
+}
+/*
+
+el codigo imprime:
+*****
+****
+***
+**
+*
+
+*/
