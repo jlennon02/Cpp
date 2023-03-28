0fe66fea39154da34ceeee14ab5e8325c6d8fae0
jlennon02 <112089009+jlennon02@users.noreply.github.com>
2023-03-27 21:55:50 -0500

    Update 2-29-cubo.cpp

--- practica_de_laboratorio_1_capitulo_2/2-29-cubo.cpp
+++ practica_de_laboratorio_1_capitulo_2/2-29-cubo.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 int main() {
 
-  cout<<"arista\tarea\tvolumen\n";
-  cout<<0<<"\t\t"<< 0 * 0<<"\t\t"<< 0 * 0 * 0<<endl ;
-  cout<< 1<<"\t\t"<< 1 * 1<<"\t\t"<< 1 * 1 * 1<<endl ;
-  cout<< 2<<"\t\t"<< 2 * 2<<"\t\t"<< 2 * 2 * 2<<endl ;
-  cout<< 3<<"\t\t"<< 3 * 3<<"\t\t"<< 3 * 3 * 3 <<endl;
-  cout<< 4<<"\t\t"<< 4 * 4<<"\t\t"<< 4 * 4 * 4 <<endl;
-
-  return 0;
+	cout << "arista\t\tarea\t\tvolumen\n";
+	for (int i = 0; i <= 4; i++) {
+		cout << i << "\t\t" << i * i << "\t\t" << i * i * i << endl;
+	}
+	return 0;
 }
